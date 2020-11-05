712df69197794f5f2540805b0313ec95f46fcb0f
Yanko Aleksandrov <superactro@gmail.com>
2020-11-05 16:26:45 +0200

    Constraints

--- query.sql
+++ query.sql
@@ -60,5 +60,67 @@ SELECT SUM(price) FROM car;
 
 SELECT make, SUM(price) FROM car GROUP BY make;
 
--- Aritmatic operations
+-- Aritmetic operations
+SELECT id, make, model, price, ROUND(price * 0.10, 2), ROUND(price - (price * 0.10), 2) FROM car;
 
+-- Alias
+SELECT id, make, model, price AS original_price, ROUND(price * 0.10, 2) AS ten_percent, ROUND(price - (price * 0.10), 2) AS discount_after_ten_percent FROM car;
+
+-- Coalesce 
+SELECT COALESCE(null, 1) AS NUMBER;
+
+SELECT COALESCE(email, 'Enail not found') FROM person;
+
+-- NULLIF
+SELECT NULLIF(10, 10);
+
+SELECT NULLIF(10, 19);
+
+SELECT COALESCE(10 / NULLIF(0, 0), 0);
+
+-- Timestamps and Dates
+SELECT NOW();
+
+SELECT NOW()::DATE;
+
+SELECT NOW()::TIME;
+
+SELECT NOW() - INTERVAL '1 YEAR';
+
+SELECT NOW() - INTERVAL '10 MONTHS';
+
+SELECT (NOW() + INTERVAL '10 DAYS')::DATE;
+
+-- Extracting Fields
+SELECT EXTRACT(YEAR FROM NOW());
+
+SELECT EXTRACT(MONTH FROM NOW());
+
+SELECT EXTRACT(DOW FROM NOW());
+
+SELECT EXTRACT(CENTURY FROM NOW());
+
+-- Age function
+SELECT first_name, last_name, gender, country_of_birth, date_of_birth, EXTRACT(YEAR FROM AGE(NOW(), date_of_birth)) AS age FROM person;
+
+-- Primary Keys
+ALTER TABLE person DROP CONSTRAINT person_pkey;
+
+ALTER TABLE person ADD PRIMARY KEY (id);
+
+DELETE FROM person WHERE id = 1;
+
+-- Unique Constraints
+SELECT email, count(*) FROM person GROUP BY email HAVING COUNT(*) > 1;
+
+SELECT * FROM person WHERE email = 'gagirre0@salon.com';
+
+ALTER TABLE person ADD CONSTRAINT unique_email_address UNIQUE (email);
+
+DELETE FROM person WHERE id = 1001;
+
+ALTER TABLE person DROP CONSTRAINT unique_email_address;
+
+ALTER TABLE person ADD UNIQUE (email);
+
+-- Check Constraints
